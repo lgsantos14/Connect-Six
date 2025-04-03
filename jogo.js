019a8e7a69fd6fcaff03e065400c74a559dc4021
Emerson Vieira <vgvieira05@academico.ufs.br>
2025-04-03 01:12:12 -0300

    Adição lógica parte 1
    Atualiza lógica de jogo no arquivo jogo.js

--- jogo.js
+++ jogo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const linhas = 7;
 const colunas = 9;
 
@@ -6,11 +7,25 @@ const criarTabuleiro = () =>
   Array.from({ length: linhas }, () => Array(colunas).fill(0));
 
 const iniciarJogadores = () => ({ jogadorAtual: 1 });
+=======
+const linhas = 7
+const colunas = 9
+
+// Criação do tabuleiro
+const criarTabuleiro = () =>
+  Array.from({ length: linhas }, () => Array(colunas).fill(0))
+
+const iniciarJogadores = () => ({ jogadorAtual: 1 })
+>>>>>>> 3d3e586 (Adição lógica)
 
 const estadoInicial = {
   tabuleiro: criarTabuleiro(),
   jogadores: iniciarJogadores(),
+<<<<<<< HEAD
 };
+=======
+}
+>>>>>>> 3d3e586 (Adição lógica)
 
 const getDiagonais = (dx, dy) =>
   Array.from({ length: linhas * colunas }, (_, i) => {
@@ -18,6 +33,7 @@ const getDiagonais = (dx, dy) =>
       c = i % colunas;
     return Array.from(
       { length: necessario },
+<<<<<<< HEAD
       (_, k) => estadoInicial.tabuleiro[r + k * dx]?.[c + k * dy] || ""
     ).join("");
   });
@@ -26,3 +42,23 @@ const getDiagonais = (dx, dy) =>
 return [...getDiagonais(1, 1), ...getDiagonais(1, -1)].some((seq) =>
   seq.includes(jogador.toString().repeat(necessario))
 );
+=======
+      (_, k) => estadoInicial.tabuleiro[r + k * dx]?.[c + k * dy] || ""  ).join("")
+  })
+
+// Verifica as diagonais
+return [...getDiagonais(1, 1), ...getDiagonais(1, -1)].some((seq) =>
+  seq.includes(jogador.toString().repeat(necessario)))
+const verificarVitoria = (jogador) =>
+  const necessario = 6
+  const temSequencia = (arr) =>
+    arr.join("").includes(jogador.toString().repeat(necessario))
+  // Verifica as linhas
+  if (estadoInicial.tabuleiro.some(temSequencia)) return true
+  // Verifica as colunas
+  if (
+    Array.from({ length: colunas }, (_, c) =>
+      estadoInicial.tabuleiro.map((linha) => linha[c])
+    ).some(temSequencia))
+    return true 
+>>>>>>> 3d3e586 (Adição lógica)
